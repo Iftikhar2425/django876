42c22ca6da32c3b0c22e79eea9d6ee1a0c23351b
Iftikhar2425 <iftikharalisyed001@gmail.com>
2025-03-02 08:07:37 +0500

    dfgdfgre

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
 
-application = get_wsgi_application()
 
-app = application
+app = get_wsgi_application()  # This is required for Vercel
+
